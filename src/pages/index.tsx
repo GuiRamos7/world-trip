9d975b50f1342aef1340e9f19e9d8561afb1e3d7
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-05-18 23:35:31 -0300

    creating some texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Flex, Text } from '@chakra-ui/react';
 import { Header, Banner, TravelTypes, Carousel } from 'components';
 
 const Home = () => {
@@ -6,6 +7,20 @@ const Home = () => {
       <Header />
       <Banner />
       <TravelTypes />
+      <Flex
+        py={['6', '6', '6', '12']}
+        w='100%'
+        justify='center'
+        direction='column'
+        align='center'
+      >
+        <Text fontWeight='semibold' fontSize={['xl', 'xl', 'xl', '4xl']}>
+          Vamos nessa?
+        </Text>
+        <Text fontWeight='semibold' fontSize={['xl', 'xl', 'xl', '4xl']}>
+          Então escolha seu continente
+        </Text>
+      </Flex>
       <Carousel />
     </>
   );
