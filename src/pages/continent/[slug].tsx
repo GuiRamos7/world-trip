1e22b79288d4b2d674d540b728a25748218ab2de
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-05-20 01:51:12 -0300

    using server side props to get data of continent

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,7 +1,47 @@
+import axios from 'axios';
 import { Header } from 'components';
+import { GetServerSideProps } from 'next';
 
-const Continent = () => {
-  return <Header />;
+interface ICountries {
+  country: string;
+  flag: string;
+  city: string;
+  image: string;
+}
+
+type ContinentInfo = {
+  title: string;
+  bio: string;
+  number_countries: number;
+  languages: number;
+  cities: number;
+  background: string;
+  countries: Array<ICountries>;
 };
 
+interface ContinentProps {
+  continentInfo: ContinentInfo;
+}
+
+const Continent = ({ continentInfo }: ContinentProps) => {
+  return (
+    <>
+      <Header />
+      {console.log(continentInfo)}
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { data } = await axios
+    .get(`http://localhost:3000/${params?.slug}`)
+    .then((el) => el);
+
+  return {
+    props: {
+      continentInfo: data,
+    },
+    redirect: 60 * 60 * 24, // 24 Hours
+  };
+};
 export default Continent;
